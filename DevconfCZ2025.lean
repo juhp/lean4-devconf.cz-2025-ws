88e57fce57cd767fa9bd302b34b40b7296fb61d1
Jens Petersen <juhpetersen@gmail.com>
2025-06-12 00:23:32 +0200

    move files under DevconfCZ2025/

--- DevconfCZ2025.lean
+++ DevconfCZ2025.lean
@@ -1,3 +1,11 @@
 -- This module serves as the root of the `DevconfCZ2025` library.
 -- Import modules here that should be built as part of the library.
-import DevconfCZ2025.Basic
+
+import DevconfCZ2025.«0_Prim»
+import DevconfCZ2025.«1_Fun»
+import DevconfCZ2025.«2_ADT»
+import DevconfCZ2025.«3_Universes»
+import DevconfCZ2025.«4_Namespaces»
+import DevconfCZ2025.«5_Dependent»
+import DevconfCZ2025.«6_Pos»
+import DevconfCZ2025.«7_IO»
